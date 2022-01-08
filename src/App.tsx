4ae3f4688ea30873c16a34e104e230474b491f97
AndreaAguado <andrea95am@gmail.com>
2022-01-08 20:57:58 +0100

    handle events in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
 import List from './components/List';
 
 interface IState {
@@ -27,6 +28,7 @@ function App() {
     <div className="App">
       <h1>People invited to my party</h1>
       <List people={people}/>
+      <AddToList/>
     </div>
   );
 }
