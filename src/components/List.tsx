c574f658f10fe4f9e08a5db8dff010bb890b2b2f
AndreaAguado <andrea95am@gmail.com>
2022-01-08 20:40:28 +0100

    render list items using function

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,10 +8,27 @@ interface IProps {
     }[]
   }
 const List: React.FC<IProps> = ({people}) => {
+
+    // JSX.Element[] is used to define the type the function returns
+    // that would trigger an error if the function didn't had  a "return"  
+    const renderList = (): JSX.Element[] => {
+        return people.map( (person) => {
+            return(
+            <li className="List">
+                <div className="List-header">
+                    <img className="List-img" src={person.url} alt={person.name} />
+                    <h2>{person.name}</h2>
+                </div>
+                <p>{person.age} years old</p>
+                <p className="List-note">{person.note}</p>
+
+            </li>)
+        })
+    }
     return(
-        <div>
-            I am list
-        </div>
+        <ul>
+            {renderList()}
+        </ul>
     )
 }
 export default List
