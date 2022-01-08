58234bb19267c4cfc4e1d74f0396909bfdb9d6a8
AndreaAguado <andrea95am@gmail.com>
2022-01-08 20:31:06 +0100

    add default item to list

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,14 @@ interface IState {
 
 function App() {
 
- const [people, setPeople] = useState<IState["people"]>([])
+ const [people, setPeople] = useState<IState["people"]>([
+   {
+     name: "Mikasa Ackerman",
+     age: 19,
+     url: "https://64.media.tumblr.com/01a3b0150ca6a02edc46eede298fd12c/a5ffc76656af69e6-57/s640x960/f3d8df3ad8b63a868211a4eabd389ba8c4bd912b.png",
+     note: "She's really cool"
+   }
+ ])
 
   return (
     <div className="App">
