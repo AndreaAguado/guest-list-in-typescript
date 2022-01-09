02a2422a6d6f71e7ba5ff8e0cb041507fd7f7afb
AndreaAguado <andrea95am@gmail.com>
2022-01-09 13:42:01 +0100

    add responsive for smaller screens

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -17,10 +17,10 @@ const List: React.FC<IProps> = ({people}) => {
     const renderList = (): JSX.Element[] => {
         return people.map( (person) => {
             return(
-            <li className="List">
+            <li className="List-item">
                 <div className="List-header">
                     <img className="List-img" src={person.url} alt={person.name} />
-                    <h2>{person.name}</h2>
+                    <h2 className="List-name">{person.name}</h2>
                 </div>
                 <p>{person.age} years old</p>
                 <p className="List-note">{person.note}</p>
@@ -29,7 +29,7 @@ const List: React.FC<IProps> = ({people}) => {
         })
     }
     return(
-        <ul>
+        <ul className="List">
             {renderList()}
         </ul>
     )
