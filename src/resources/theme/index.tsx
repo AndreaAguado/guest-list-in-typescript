e6e41596aa5fd9b083eb5c87950b91932b298bc8
AndreaAguado <andrea95am@gmail.com>
2022-01-19 20:55:12 +0100

    change input bg color according to mode

--- src/resources/theme/index.tsx
+++ src/resources/theme/index.tsx
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/react'
+import {mode} from '@chakra-ui/theme-tools'
 
 const colors = {
     brand: {
@@ -6,4 +7,37 @@ const colors = {
     },
 }
 
-export const theme = extendTheme({ colors })
+const Input = {
+    // style object for base or default style
+    baseStyle: {
+        colorScheme: 'red',
+    },
+    // styles for different sizes ("sm", "md", "lg")
+    sizes: {},
+    // styles for different visual variants ("outline", "solid")
+    variants: {
+        filled: (props: any) => ({
+            field: {
+                bgColor: mode('white', 'pink')(props),
+                _focus: {
+                    bg: mode('white', 'pink')(props)
+                },
+                _hover: {
+                    bg: mode('teal.50', 'pink.300')(props),
+                },
+                _placeholder: { 
+                    color: mode('grey', 'white')(props),
+                }
+            },
+    
+            
+        })
+    },
+    // default values for `size` and `variant`
+    defaultProps: {
+      size: '',
+      variant: '',
+    },
+  }
+
+export const theme = extendTheme({ colors, components: { Input } })
