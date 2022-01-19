b9e8a2c86d2feb15e3af618a037ef411cee83cbd
AndreaAguado <andrea95am@gmail.com>
2022-01-19 22:27:16 +0100

    extend textarea theme

--- src/resources/theme/index.tsx
+++ src/resources/theme/index.tsx
@@ -55,4 +55,23 @@ const Input = {
     },
   }
 
-export const theme = extendTheme({ colors, components: { Input, NumberInput } })
+  const Textarea = {
+    variants: {
+        filled: (props: any) => ({
+            bgColor: mode('white', 'pink')(props),
+            fontWeight: 'bold',
+            _focus: {
+                bg: mode('white', 'pink')(props)
+            },
+            _hover: {
+                bg: mode('teal.50', 'pink.300')(props),
+            },
+            _placeholder: { 
+                color: mode('grey', 'white')(props),
+                fontWeight: 'light',
+            }  
+        })
+    },
+  }
+
+export const theme = extendTheme({ colors, components: { Input, NumberInput, Textarea } })
